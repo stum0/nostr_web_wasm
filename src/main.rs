34cdd6cb6e24a95e2dde6527120c75074684b519
stu <stuart.morley@zebedee.io>
2023-01-31 13:50:01 +0000

    added more exampled draft

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@ async fn async_main() {
     debug!("{:?}", nostr_pub_key);
 }
 
+//get pub key example
 #[wasm_bindgen(inline_js = "export async function pub_key() { 
     const encoder = new TextEncoder()
 const view = encoder.encode(await window.nostr.getPublicKey())
@@ -27,3 +28,84 @@ return view;
 extern "C" {
     async fn pub_key() -> JsValue;
 }
+
+
+//encrypt message example
+// #[wasm_bindgen(inline_js = "export async function encrypt_msg(pubkey, plaintext) { 
+//     const encoder = new TextEncoder()
+//     const view = encoder.encode(
+//         await window.nostr.nip04.encrypt(pubkey, plaintext))
+//     return view;
+// }
+//  ")]
+// extern "C" {
+//     async fn encrypt_msg(pubkey: String, ciphertext: String) -> wasm_bindgen::JsValue;
+// }
+
+
+//decrypt message example
+// #[wasm_bindgen(inline_js = "export async function decrypt_msg(pubkey, ciphertext) { 
+//     const encoder = new TextEncoder()
+//     const view = encoder.encode(
+//         await window.nostr.nip04.decrypt(pubkey, ciphertext))
+//     return view;
+// }
+//  ")]
+// extern "C" {
+//     async fn decrypt_msg(pubkey: String, ciphertext: String) -> wasm_bindgen::JsValue;
+// }
+
+
+//sign event example 
+
+// #[derive(Serialize, Deserialize, Debug)]
+// pub struct NostrEv {
+//     pub id: Option<String>,
+//     pub kind: u32,
+//     pub pubkey: Option<String>,
+//     pub content: String,
+//     pub tags: Vec<nostr::Tag>,
+//     created_at: u64,
+//     pub sig: Option<String>,
+// }
+
+// pub async fn send_to_js(content: String) -> NostrEv {
+//     let nostr_tag = "0905c0ff-9da3-48c7-906e-88883fdf14b3".to_string();
+
+    
+//     let created_at = timestamp();
+//     let tags = vec![Tag::Generic(
+//         TagKind::Custom("t".to_string()),
+//         vec![nostr_tag],
+//     )];
+
+//     let nostr_ev = NostrEv {
+//         id: None,
+//         kind: 1,
+//         pubkey: None,
+//         content,
+//         tags,
+//         created_at,
+//         sig: None,
+//     };
+
+//     let event = serde_wasm_bindgen::to_value(&nostr_ev).unwrap();
+
+//     let signed_event = make_event(event).await;
+
+//     let event: NostrEv = JsValue::into_serde(&signed_event).unwrap();
+
+//     event
+    
+
+// }
+
+// #[wasm_bindgen(inline_js = "export async function make_event(Event) { 
+//     const event = await window.nostr.signEvent(Event)
+  
+//     return event;
+// }
+//  ")]
+// extern "C" {
+//     async fn make_event(event: JsValue) -> wasm_bindgen::JsValue;
+// }
